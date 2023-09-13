838ed4137742eebd04a43af87a372d99fd7e1be0
Thomas Boudin <thomasboudin@hotmail.fr>
2023-09-13 16:12:06 +0200

    ajout de la partie filtre et modifications css

--- FrontEnd/JavaScript/datas.js
+++ FrontEnd/JavaScript/datas.js
@@ -1,14 +1,35 @@
 const reponse = await fetch("http://localhost:5678/api/works");
 const works = await reponse.json();
 
-function getWorks(works) {
-    for (let i = 0; i < works.length; i++) {
-        const work = works[i];
+//const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json());
 
-        const sectionWorks = document.querySelector(".gallery");
+const sectionWorks = document.querySelector(".gallery");
+
+const objets = works.filter((work) => {
+    return work.categoryId === 1;
+});
+
+const appartements = works.filter((work) => {
+    return work.categoryId === 2;
+});
+
+const hotelsEtRestaurants = works.filter((work) => {
+    return work.categoryId === 3;
+});
+
+
+//const workCategorieId = works.map(work => work.categoryId);
+
+
+function getWorks(currentWorks) {
+
+
+    for (let i = 0; i < currentWorks.length; i++) {
+
+        const work = currentWorks[i];
 
         const workElement = document.createElement("figure");
-        workElement.dataset.id = works[i].id;
+        workElement.dataset.id = currentWorks[i].id;
 
         const imageElement = document.createElement("img");
         imageElement.src = work.imageUrl;
@@ -20,10 +41,64 @@ function getWorks(works) {
         workElement.appendChild(imageElement);
         workElement.appendChild(nomElement);
 
-    }
-}
+    };
+};
 
-//function filter(works) {}
 
-getWorks(works)
+getWorks(works);
+
+
+
+const boutonsFiltre = document.querySelectorAll(".filtre button");
+
+for (let i = 0; i < boutonsFiltre.length; i++) {
+    boutonsFiltre[i].addEventListener("click", (event) => {
+        const IdBouton = event.target.id;
+        console.log(IdBouton)
+        switch (IdBouton) {
+            case '0':
+                sectionWorks.innerHTML = "";
+                getWorks(works)
+                break
+            case '1':
+                sectionWorks.innerHTML = "";
+                getWorks(objets)
+                break
+            case '2':
+                sectionWorks.innerHTML = "";
+                getWorks(appartements)
+                break
+            case '3':
+                sectionWorks.innerHTML = "";
+                getWorks(hotelsEtRestaurants)
+                break
+                
+        };
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+//getCategoryId()
+
+/* const categoryObjets = works.filter(function (workCategory){
+    return workCategory.categoryId === 1;
+})
+console.log(categoryObjets)
+
+const categoryAppartements = works.filter(function (workCategory){
+    return workCategory.categoryId === 2;
+})
+console.log(categoryAppartements) */
+
 
